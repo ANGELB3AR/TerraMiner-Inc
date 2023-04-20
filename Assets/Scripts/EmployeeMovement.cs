3da192ea81407348d1ef12494bb99596136db386
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-04-20 06:13:17 -0500

    Extracted clicking behavior

--- Assets/Scripts/EmployeeMovement.cs
+++ Assets/Scripts/EmployeeMovement.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using System;
 using UnityEngine.AI;
 
@@ -10,26 +9,11 @@ public class EmployeeMovement : MonoBehaviour
     [SerializeField] NavMeshAgent agent = null;
     [SerializeField] Animator animator = null;
 
-    Camera mainCamera;
-
     readonly int walkingVelocity = Animator.StringToHash("Velocity");
-
-    private void Awake()
-    {
-        mainCamera = Camera.main;
-    }
 
     private void Update()
     {
         animator.SetFloat(walkingVelocity, agent.velocity.magnitude);
-
-        if (!Mouse.current.leftButton.wasPressedThisFrame) { return; }
-
-        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-
-        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) { return; }
-
-        Move(hit.point);
     }
 
     private void Move(Vector3 point)
--- Assets/Scripts/EmployeeSelection.cs
+++ Assets/Scripts/EmployeeSelection.cs
@@ -1,8 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EmployeeSelection : MonoBehaviour
 {
+    [SerializeField] List<GameObject> selectedEmployees = new List<GameObject>();
+    Camera mainCamera;
 
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (!Mouse.current.leftButton.wasPressedThisFrame) { return; }
+
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) { return; }
+
+        //Move(hit.point);
+    }
 }
