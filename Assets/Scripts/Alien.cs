993b3e78e6ec1e78feb7b648ee5f862619baa488
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-05-11 07:12:10 -0500

    Started setting up the first 2 alien types

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -1,8 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Alien : MonoBehaviour
 {
+    [SerializeField] NavMeshAgent agent = null;
+    [SerializeField] Animator animator = null;
+
+    /*
+     WANDERING
+     FIGHTING
+    DESTROYING EQUIPMENT
+     */
+
 
 }
