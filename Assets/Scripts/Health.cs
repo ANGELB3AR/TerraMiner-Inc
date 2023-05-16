4c8eb312817ff84487362d07ba65db3e5176949b
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-05-16 06:25:22 -0500

    Started health system

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int maxHealth = 100;
+
+    int currentHealth;
+
+    public event Action<int, int> OnHealthUpdated;
+    public event Action OnDied;
+
+    public int GetMaxHealth()
+    {
+        return maxHealth;
+    }
+
+    public int GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    public void DealDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        OnHealthUpdated?.Invoke(currentHealth, maxHealth);
+
+        if (currentHealth != 0) { return; }
+
+        Die();
+    }
+
+    public void Heal(int healAmount)
+    {
+        currentHealth += healAmount;
+        Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        OnHealthUpdated?.Invoke(currentHealth, maxHealth);
+    }
+
+    void Die()
+    {
+        OnDied?.Invoke();
+    }
+}
