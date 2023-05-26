5d4f0f08da72684805f8818b81f8cb5427be4627
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-05-26 12:46:28 -0500

    Wrote abstract State class

--- Assets/Scripts/State Machines/State.cs
+++ Assets/Scripts/State Machines/State.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class State : MonoBehaviour
+public abstract class State
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public abstract void Enter();
+    public abstract void Tick(float deltaTime);
+    public abstract void Exit();
 }
